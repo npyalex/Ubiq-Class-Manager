d88cbe90c214af52538a16d9a3ab42a0006caa21
npyalex <np.alexander@gmail.com>
2019-01-25 15:07:07 -0500

    Update Student_Node.ino

--- Student_Node/Student_Node.ino
+++ Student_Node/Student_Node.ino
@@ -1,14 +1,15 @@
 const int ledPin = A0;
 const int buttonPin = 4;
 int buttonState;
-int incomingByte;
-int studentNum = 6; // set your radio number to this 
+char incomingByte;
+int studentNum = 6; // this should be the same as your ATMY
 bool myTurn = false;
 bool isDataSent = false;
 
 void setup() {
   // put your setup code here, to run once:
   Serial1.begin(9600);
+//  Serial.begin(9600);
   pinMode (ledPin, OUTPUT);
   pinMode (buttonPin, INPUT);
 }
@@ -16,30 +17,38 @@ void setup() {
 void loop() {
   queue();
   attend();
-  if (myTurn == true) {
-    digitalWrite(ledPin, HIGH);
-    } else {
-    digitalWrite(ledPin, LOW);
-    }
+//  if (myTurn == true) {
+
 }
 
 void attend() {
     if(Serial1.available() > 0) {
       incomingByte = Serial1.read();
-    if(incomingByte == '1'){
-      myTurn = true;
+          if(Serial.available() > 0) {
+            Serial.print(incomingByte);
+          
+    if(incomingByte == '1') {
+    digitalWrite(ledPin, HIGH);
+    } else if (incomingByte == '0') {
+    digitalWrite(ledPin, LOW);
     }
-    if(incomingByte == '0'){
-      myTurn = false;
     }
-  }
-}
+//    if(incomingByte == '1'){
+//      myTurn = true;
+//    }
+//    if(incomingByte == '0'){
+//      myTurn = false;
+    }
+ }
+
 
 void queue() {
   buttonState = digitalRead(buttonPin);
   if (buttonState == LOW && isDataSent == false) {
     Serial1.println(studentNum);
     Serial.println(studentNum);
+    delay(1);
+    Serial1.println('X');
     isDataSent = true;
   }
   else if ( buttonState == HIGH) {
