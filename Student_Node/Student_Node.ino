8e8afd67a45e936422c4c66c3ca12be401c8f904
npyalex <np.alexander@gmail.com>
2019-01-25 13:06:13 -0500

    queue & attend
    Listen to the teacher & put your hand up when you want to talk

--- Student_Node/Student_Node.ino
+++ Student_Node/Student_Node.ino
@@ -1,17 +1,34 @@
-int ledPin = A0;
+const int ledPin = A0;
+const int buttonPin = 4;
+int buttonState;
 
 void setup() {
   // put your setup code here, to run once:
   Serial1.begin(9600);
   pinMode (ledPin, OUTPUT);
+  pinMode (buttonPin, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if(Serial1.available() > 0) {
-    incomingByte = Serial1.read()();
+  queue();
+  attend();
+  }
+
+void attend() {
+    if(Serial1.available() > 0) {
+      incomingByte = Serial1.read()();
     if(incomingByte == '1'){
-      digitalWrite
+      digitalWrite(ledPin, HIGH);
     }
+    if(incomingByte == '0'){
+      digitalWrite(ledPin, LOW);
+    }
+  }
+}
+
+void queue() {
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH) {
+    Serial1.print('H');
   }
 }
