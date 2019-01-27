5d53305ff1b533bbbc43ca5dc3dd2fa1279a02fc
npyalex <np.alexander@gmail.com>
2019-01-27 18:57:22 -0500

    Cleanup
    Cleaned up and prepared for next iteration of Master

--- Student_Node/Student_Node.ino
+++ Student_Node/Student_Node.ino
@@ -3,13 +3,11 @@ const int buttonPin = 4;
 int buttonState;
 char incomingByte;
 char studentNum = '5'; // this should be the same as your ATMY
-bool myTurn = false;
 bool isDataSent = false;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial1.begin(9600);
-//  Serial.begin(9600);
+  Serial.begin(9600);
   pinMode (ledPin, OUTPUT);
   pinMode (buttonPin, INPUT_PULLUP);
 }
@@ -17,29 +15,23 @@ void setup() {
 void loop() {
   queue();
   attend();
-//  if (myTurn == true) {
-    Serial.println(incomingByte);
 }
 
 void attend() {
     if(Serial1.available() > 0) {
       incomingByte = Serial1.read();
+      Serial1.print(incomingByte);
           if(incomingByte == studentNum) {
           digitalWrite(ledPin, HIGH);
     } 
-      if (incomingByte == '0') {
+      if (incomingByte != studentNum) {
           digitalWrite(ledPin, LOW);
     }
       if(Serial.available() > 0) {
             Serial.print(incomingByte);
     }
-//    if(incomingByte == '1'){
-//      myTurn = true;
-//    }
-//    if(incomingByte == '0'){
-//      myTurn = false;
-    }
- }
+  }
+}
 
 
 void queue() {
@@ -50,11 +42,9 @@ void queue() {
 //    Serial1.println(studentNum);
     Serial1.println(studentNum);
     Serial.println(studentNum);
-//    delay(1);
-//    Serial1.println('X');
     isDataSent = true;
   }
-  else if ( buttonState == LOW) {
+  if ( buttonState == LOW) {
     isDataSent = false;
   }
 }
