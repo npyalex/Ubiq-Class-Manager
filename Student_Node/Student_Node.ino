bd9b73dc33448e3ce449e587953fb25f6a34190d
Omid <omid_ettehadi@yahoo.com>
2019-01-28 19:55:21 -0500

    added handshakes between the users

--- Student_Node/Student_Node.ino
+++ Student_Node/Student_Node.ino
@@ -1,13 +1,17 @@
-const int ledPin = A0;
-const int buttonPin = 4;
+#define ledPin A0
+#define buttonPin 4
+
+const char studentNum = '4'; // this should be your radio's ATMY
+
 int buttonState;
-char incomingByte;
-char studentNum = '5'; // this should be the same as your ATMY
+char incomingByte = 0;
 bool isDataSent = false;
 
 void setup() {
+  // Serial Connections
   Serial1.begin(9600);
   Serial.begin(9600);
+  // Pin Assignment
   pinMode (ledPin, OUTPUT);
   pinMode (buttonPin, INPUT_PULLUP);
 }
@@ -18,34 +22,22 @@ void loop() {
 }
 
 void attend() {
-    if(Serial1.available() > 0) {
-      incomingByte = Serial1.read();
-      Serial1.print(incomingByte);
-      Serial.print(incomingByte);
-          if(incomingByte == studentNum) {
-          digitalWrite(ledPin, HIGH);
-    } 
-      if (incomingByte != studentNum) {
-          digitalWrite(ledPin, LOW);
-    }
- //     if(Serial.available() > 0) {
- //           Serial.print(incomingByte);
- //   }
+  if(Serial1.available() > 0) {
+    incomingByte = Serial1.read();
+    Serial1.print(incomingByte);
+    //Serial.print(incomingByte);
+    if (incomingByte == studentNum) { digitalWrite(ledPin, HIGH); } 
+    else if (incomingByte != studentNum) { digitalWrite(ledPin, LOW); }
   }
 }
 
 
 void queue() {
   buttonState = digitalRead(buttonPin);
-  if (buttonState == HIGH && isDataSent == false) {
-//    Serial1.println(studentNum);
-//    Serial1.println(studentNum);
-//    Serial1.println(studentNum);
-    Serial1.println(studentNum);
-    Serial.println(studentNum);
-    isDataSent = true;
-  }
-  if ( buttonState == LOW) {
-    isDataSent = false;
+  if (buttonState == LOW && isDataSent == false) {
+    while(Serial1.read() != 'Y'){Serial1.println(studentNum); delay(200);}
+    isDataSent == true;
+  } else if (buttonState == HIGH ){
+    isDataSent == false;
   }
 }
--- Teacher_Node/Teacher_Node.ino
+++ Teacher_Node/Teacher_Node.ino
@@ -1,81 +1,90 @@
 #define buttonPin 4
 
 int buttonState;
-char incomingByte;
-
-int number = 0;
-int queue[50];
-
 bool isButtonPressed = false;
+char incomingByte;
+int queue[50];
+int place = 0;
 
 void setup() {
-  // put your setup code here, to run once:
+  // Serial Connections
   Serial1.begin(9600);
   Serial.begin(9600);
-  
+  // Pin Assignment
   pinMode (buttonPin, INPUT_PULLUP);
-
+  // Initialize queue
   for (int i = 0 ; i < 50 ; i++){
     queue[i] = 0;
   }
 }
 
 void loop() {
+  // Check for users and add them to queue
   if(Serial1.available() > 0) {
     incomingByte = Serial1.read();
     // Olivia
     if(incomingByte == '6') {
-      //Serial.print(number);
-      //Serial.println("Olivia");
-      queue[number] = 6;
-      
-      Serial.println(queue[number]);
-      number++;
-      /*Serial1.println(6);
-      delay(1000);*/
+      Serial1.print('Y');
+      // only add them if they aren't the last person on the queue
+      if ( queue[place-1] != 6 ) {      
+        queue[place] = 6;
+        Serial.println(queue[place]);
+        place++;
+      }
     }
     // Nick
     else if(incomingByte == '5') {
-      //Serial.print(number);
-      //Serial.println("Nick");
-      queue[number] = 5;
-      
-      Serial.println(queue[number]);
-      number++;
-      /*Serial1.println(5);
-      delay(1000);*/
+      Serial1.print('Y');
+      // only add them if they aren't the last person on the queue
+      if ( queue[place-1] != 5 ) {      
+        queue[place] = 5;
+        Serial.println(queue[place]);
+        place++;
+      }
     }
     // Carissa
     else if(incomingByte == '4') {
-      //Serial.print(number);
-      //Serial.println("Carisa");
-      queue[number] = 4;
-      
-      Serial.println(queue[number]);
-      number++;
-      /*Serial1.println(4);
-      delay(1500);*/
+      Serial1.print('Y');
+      // only add them if they aren't the last person on the queue
+      if ( queue[place-1] != 4 ) {      
+        queue[place] = 4;
+        Serial.println(queue[place]);
+        place++;
+      }
     }
   }
+
+  // Turn on the LED of the person who should talk
   if ( digitalRead(buttonPin) == LOW && isButtonPressed == false) {
-    Serial1.print('0');
-    delay(500);
+    isButtonPressed = true;
     char x;
     if ( queue[0] == 4){
       x = '4';
+      Serial1.print(x);
+      Serial.println(x);
+      for ( int i = 0 ; i < 50 ; i++){
+        queue[i] = queue[i+1];
+      }
+      while (Serial1.read() !=  '4') { delay(100); }
     } else if ( queue[0] == 5 ){
       x = '5';
+      Serial1.print(x);
+      Serial.println(x);
+      for ( int i = 0 ; i < 50 ; i++){
+        queue[i] = queue[i+1];
+      }
     } else if ( queue[0] == 6) {
       x = '6';
+      Serial1.print(x);
+      Serial.println(x);
+      for ( int i = 0 ; i < 50 ; i++){
+        queue[i] = queue[i+1];
+      }
     } else {
+      place = 0;
       x = '0';
-    }
-    Serial.print(x);
-    Serial1.print(x);
-    isButtonPressed = true;
-
-    for ( int i = 0 ; i < 50 ; i++){
-      queue[i] = queue[i+1];
+      Serial1.print(x);
+      Serial.println("no one in queue");
     }
     delay(500);
   } else if ( digitalRead(buttonPin) == HIGH ) {
