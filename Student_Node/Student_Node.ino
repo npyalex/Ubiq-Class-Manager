8a6a188386257abb61f9cb0695db1b51dc7948bd
Omid <omid_ettehadi@yahoo.com>
2019-01-29 12:02:57 -0500

    Go back to the last working state

--- Student_Node/Student_Node.ino
+++ Student_Node/Student_Node.ino
@@ -1,17 +1,13 @@
-#define ledPin A0
-#define buttonPin 4
-
-const char studentNum = '4'; // this should be your radio's ATMY
-
+const int ledPin = A0;
+const int buttonPin = 4;
 int buttonState;
-char incomingByte = 0;
+char incomingByte;
+char studentNum = '5'; // this should be the same as your ATMY
 bool isDataSent = false;
 
 void setup() {
-  // Serial Connections
   Serial1.begin(9600);
   Serial.begin(9600);
-  // Pin Assignment
   pinMode (ledPin, OUTPUT);
   pinMode (buttonPin, INPUT_PULLUP);
 }
@@ -22,22 +18,34 @@ void loop() {
 }
 
 void attend() {
-  if(Serial1.available() > 0) {
-    incomingByte = Serial1.read();
-    Serial1.print(incomingByte);
-    //Serial.print(incomingByte);
-    if (incomingByte == studentNum) { digitalWrite(ledPin, HIGH); } 
-    else if (incomingByte != studentNum) { digitalWrite(ledPin, LOW); }
+    if(Serial1.available() > 0) {
+      incomingByte = Serial1.read();
+      Serial1.print(incomingByte);
+      Serial.print(incomingByte);
+          if(incomingByte == studentNum) {
+          digitalWrite(ledPin, HIGH);
+    } 
+      if (incomingByte != studentNum) {
+          digitalWrite(ledPin, LOW);
+    }
+ //     if(Serial.available() > 0) {
+ //           Serial.print(incomingByte);
+ //   }
   }
 }
 
 
 void queue() {
   buttonState = digitalRead(buttonPin);
-  if (buttonState == LOW && isDataSent == false) {
-    while(Serial1.read() != 'Y'){Serial1.println(studentNum); delay(200);}
-    isDataSent == true;
-  } else if (buttonState == HIGH ){
-    isDataSent == false;
+  if (buttonState == HIGH && isDataSent == false) {
+//    Serial1.println(studentNum);
+//    Serial1.println(studentNum);
+//    Serial1.println(studentNum);
+    Serial1.println(studentNum);
+    Serial.println(studentNum);
+    isDataSent = true;
+  }
+  if ( buttonState == LOW) {
+    isDataSent = false;
   }
 }
